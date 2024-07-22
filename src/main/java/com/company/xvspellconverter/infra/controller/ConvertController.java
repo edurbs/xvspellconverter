25b972d58065c0665b7b6e4942d1a3e85f4025db
Eduardo Soares <edurbs@gmail.com>
2024-07-22 11:27:54 -0300

    remove imports

--- src/main/java/com/company/xvspellconverter/infra/controller/ConvertController.java
+++ src/main/java/com/company/xvspellconverter/infra/controller/ConvertController.java
@@ -3,7 +3,6 @@ package com.company.xvspellconverter.infra.controller;
 import com.company.xvspellconverter.app.usecase.ConvertUseCase;
 import io.jmix.rest.annotation.RestMethod;
 import io.jmix.rest.annotation.RestService;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestService("convert")
